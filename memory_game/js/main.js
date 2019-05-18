aabef17fd86c0fdc82d88bea1edf091cbeb4b895
Dustin <someone@someplace.com>
2019-05-17 19:09:24 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,14 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var CardThree = "king";
-var CardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = (cards[0]);
+var cardTwo = (cards[2]);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardOne === cardTwo) {
+	alert("You found a match!");}
+else {
+	alert("Sorry, try again.");
+}
 console.log("User Flipped " + cardOne);
 console.log("User Flipped " + cardTwo);
