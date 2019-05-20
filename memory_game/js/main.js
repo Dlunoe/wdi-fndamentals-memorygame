6328668731ba20f79a159c1d25f99c2b57296acb
Dustin <someone@someplace.com>
2019-05-19 19:56:14 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,39 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-cardsInPlay.push(cards);
+const cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png',
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png',
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png',
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png',
+}
+];
+const cardsInPlay = [];
+cardsInPlay.push(cards.rank);
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");}
-else {
+	console.log("You found a match!");
+} else {
 	console.log("Sorry, try again.");
 }
 };
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log (cards[cardId].cardImage);
+	console.log (cards[cardId].suit);
 	checkForMatch();
 };
 flipCard(0);
